5c17582a411aa27b54f5d24d5dbf4963233a8c8d
pdharme <dharme.p@husky.neu.edu>
2021-07-10 15:22:37 -0400

    Adding Login button and home screen activity

--- app/src/main/java/edu/neu/madcourse/myapplication/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/LoginActivity.java
@@ -1,6 +1,9 @@
 package edu.neu.madcourse.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // called upon clicking Login button
+    public void onClickLoginButton(View view) {
+        System.out.println("Login button clicked");
+
+        TextView usernameTextView = (TextView) findViewById(R.id.editUsername);
+        String username = usernameTextView.getText().toString();
+
+        if (!userExists(username)) {
+            createNewUser(username);
+        }
+
+        startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
+    }
+
+    // saves the new user to database
+    private void createNewUser(String username) {
+    }
+
+    // returns True if user exists, else False
+    private boolean userExists(String username) {
+        // write an api call to fetch if user exists
+        return true;
     }
 }
