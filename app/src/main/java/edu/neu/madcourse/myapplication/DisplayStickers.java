b93aca8ec116f0b3895e013ea9b0599451e9bede
musembik <77795930+musembik@users.noreply.github.com>
2021-07-10 15:10:06 -0400

    Added display stickers activity

---
+++ app/src/main/java/edu/neu/madcourse/myapplication/DisplayStickers.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayStickers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_stickers);
+    }
+}
