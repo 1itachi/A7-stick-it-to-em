8c6c41022c86cf0fe1ada44c0c59f2640be2b06b
Prajakta Prashant Dharme <pdharme7@gmail.com>
2021-07-10 16:00:09 -0400

    Temporarily linked DisplayStickers activity to the home screen (#6)
    Co-authored-by: pdharme <dharme.p@husky.neu.edu>

--- app/src/main/java/edu/neu/madcourse/myapplication/HomeScreenActivity.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/HomeScreenActivity.java
@@ -1,6 +1,9 @@
 package edu.neu.madcourse.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,11 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    // called upon clicking onClickSeeAllStickers button
+    public void onClickSeeAllStickersButton(View view) {
+        System.out.println("SeeAllStickers button clicked");
+        startActivity(new Intent(HomeScreenActivity.this, DisplayStickers.class));
     }
 }
