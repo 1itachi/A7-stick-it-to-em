07f66be67f6b1e07ce932c0b5ac8f85f5db5d11a
pdharme <dharme.p@husky.neu.edu>
2021-07-11 12:35:47 -0400

    Adding onClick to image button

--- app/src/main/java/edu/neu/madcourse/myapplication/DisplayStickers.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/DisplayStickers.java
@@ -2,8 +2,11 @@ package edu.neu.madcourse.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayStickers extends AppCompatActivity {
     private String current_user_username;
@@ -18,6 +21,16 @@ public class DisplayStickers extends AppCompatActivity {
         friend_username = getIntent().getStringExtra("friend_username");
         TextView send_sticker_info = (TextView) findViewById(R.id.sendStickerInfo);
         send_sticker_info.append("Send any of these stickers to " + friend_username);
+
+    }
+
+    // called upon clicking any image
+    public void onClickImageButton(View view) {
+        String sticker_tag = (String)view.getTag();
+        System.out.println("Image clicked for tag " + sticker_tag);
+
+        Toast myToast = Toast.makeText(this, "Sticker sent!", Toast.LENGTH_LONG);
+        myToast.show();
 
     }
 }
