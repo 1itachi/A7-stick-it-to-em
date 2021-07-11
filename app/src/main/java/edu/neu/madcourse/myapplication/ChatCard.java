864d30622593d03a43fcf2349ce4c8e6f214d1e6
Deepak Kumar <deepak94.mce@gmail.com>
2021-07-11 13:15:01 -0400

    change chart card (#25)

--- app/src/main/java/edu/neu/madcourse/myapplication/ChatCard.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/ChatCard.java
@@ -3,30 +3,48 @@ package edu.neu.madcourse.myapplication;
 public class ChatCard {
     private String sender;
     private String receiver;
-    private String Sticker;
+    private String sticker;
     private String time;
+
+    public ChatCard(){}
 
     public ChatCard(String sender, String receiver, String sticker, String time) {
         this.sender = sender;
         this.receiver = receiver;
-        Sticker = sticker;
+        this.sticker = sticker;
         this.time = time;
     }
 
     public String getSender() {
-        return sender;
+        return this.sender;
     }
 
     public String getReceiver() {
-        return receiver;
+        return this.receiver;
     }
 
     public String getSticker() {
-        return Sticker;
+        return this.sticker;
     }
 
     public String getTime() {
-        return time;
+        return this.time;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setSticker(String sticker) {
+        this.sticker = sticker;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
 }
