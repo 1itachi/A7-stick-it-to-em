9b2858c8611deae0e21e3b1ef246bda6f2b54697
pdharme <dharme.p@husky.neu.edu>
2021-07-11 18:43:07 -0400

    Adding hashmap to store sticker ids

--- app/src/main/java/edu/neu/madcourse/myapplication/RecyclerViewAdapterChatHistory.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/RecyclerViewAdapterChatHistory.java
@@ -14,18 +14,39 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 public class RecyclerViewAdapterChatHistory extends RecyclerView.Adapter<RecyclerViewAdapterChatHistory.RecyclerViewHolder>{
 
     private ArrayList<ChatCard> chat_card_list;
     private String current_user_username, friend_username;
+    private HashMap<String, Integer> sticker_id_mapping;
 
     public RecyclerViewAdapterChatHistory(ArrayList<ChatCard> chat_card_list,
                                           String current_user_username, String friend_username) {
         this.chat_card_list = chat_card_list;
         this.current_user_username = current_user_username;
         this.friend_username = friend_username;
+        this.sticker_id_mapping = new HashMap<>();
+        map_sticker_ids();
+    }
+
+    private void map_sticker_ids() {
+        sticker_id_mapping.put("sticker1", R.drawable.sticker1);
+        sticker_id_mapping.put("sticker2", R.drawable.sticker2);
+        sticker_id_mapping.put("sticker3", R.drawable.sticker3);
+        sticker_id_mapping.put("sticker4", R.drawable.sticker4);
+        sticker_id_mapping.put("sticker5", R.drawable.sticker5);
+        sticker_id_mapping.put("sticker6", R.drawable.sticker6);
+        sticker_id_mapping.put("sticker7", R.drawable.sticker7);
+        sticker_id_mapping.put("sticker8", R.drawable.sticker8);
+        sticker_id_mapping.put("sticker9", R.drawable.sticker9);
+        sticker_id_mapping.put("sticker10", R.drawable.sticker10);
+        sticker_id_mapping.put("sticker12", R.drawable.sticker12);
+        sticker_id_mapping.put("sticker13", R.drawable.sticker13);
+        sticker_id_mapping.put("sticker14", R.drawable.sticker14);
+        sticker_id_mapping.put("sticker15", R.drawable.sticker15);
     }
 
     @NonNull
@@ -40,7 +61,7 @@ public class RecyclerViewAdapterChatHistory extends RecyclerView.Adapter<Recycle
     public void onBindViewHolder(@NonNull RecyclerViewAdapterChatHistory.RecyclerViewHolder holder, int position) {
         ChatCard chatCard = chat_card_list.get(position);
 
-        int sticker_id = chatCard.getSticker_id();
+        String sticker_tag = chatCard.getSticker();
         String time = chatCard.getTime();
         Long value = Long.parseLong(time);
         Date date = new Date(value);
@@ -48,15 +69,14 @@ public class RecyclerViewAdapterChatHistory extends RecyclerView.Adapter<Recycle
         format.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
         String formatted = format.format(date);
 
-
         String sender = chatCard.getSender();
         String receiver = chatCard.getReceiver();
 
         if (sender.equals(current_user_username) && receiver.equals(friend_username)) {
-            holder.senderSticker.setImageResource(sticker_id);
+            holder.senderSticker.setImageResource(sticker_id_mapping.get(sticker_tag));
             holder.senderStickerTime.setText(formatted);
         } else if(sender.equals(friend_username) && receiver.equals(current_user_username)) {
-            holder.receiverSticker.setImageResource(sticker_id);
+            holder.receiverSticker.setImageResource(sticker_id_mapping.get(sticker_tag));
             holder.receiverStickerTime.setText(formatted);
         }
     }
