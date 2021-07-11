aabc75f60175a332d58018032b9b801fb8646394
Deepak Kumar <deepak94.mce@gmail.com>
2021-07-11 11:46:53 -0400

    add user (#19)
    Co-authored-by: Prajakta Prashant Dharme <pdharme7@gmail.com>

--- app/src/main/java/edu/neu/madcourse/myapplication/UserCard.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/UserCard.java
@@ -3,12 +3,18 @@ package edu.neu.madcourse.myapplication;
 public class UserCard {
     private String username;
 
+    public UserCard(){}
+
     public UserCard(String username) {
         this.username = username;
     }
 
     public String getUsername() {
         return username;
+    }
+  
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
 
